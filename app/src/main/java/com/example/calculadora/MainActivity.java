0b0a5b9a0131838e113bd0cff38a074a1bd515a4
Andre Barreto <andrebarreto182@gmail.com>
2022-03-09 15:23:58 -0300

    calculadora

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText numero1;
+    private EditText numero2;
+    private TextView resultado;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        numero1 = findViewById(R.id.numero1);
+        numero2 = findViewById(R.id.numero2);
+        resultado = findViewById(R.id.resultado);
+    }
+
+    public void somar(View view) {
+        int valor1 = Integer.parseInt(numero1.getText().toString());
+        int valor2 = Integer.parseInt(numero2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 + valor2));
+    }
+
+    public void subtrair(View view) {
+        int valor1 = Integer.parseInt(numero1.getText().toString());
+        int valor2 = Integer.parseInt(numero2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 - valor2));
+    }
+
+    public void multiplicar(View view) {
+        int valor1 = Integer.parseInt(numero1.getText().toString());
+        int valor2 = Integer.parseInt(numero2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 * valor2));
+    }
+
+    public void dividir(View view) {
+        int valor1 = Integer.parseInt(numero1.getText().toString());
+        int valor2 = Integer.parseInt(numero2.getText().toString());
+
+        resultado.setText(String.valueOf(valor1 / valor2));
+    }
+}
